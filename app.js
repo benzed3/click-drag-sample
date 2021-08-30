e086eea27572d9d510465bd042426584a6314009
benzed3 <benzed3@gmail.com>
2021-08-30 15:08:40 -0400

    code for drag and drop added

--- app.js
+++ app.js
@@ -37,3 +37,19 @@ blackrook.addEventListener("touchend", function (ev) {
     var y = parseInt(blackrook.style.top);
 
 })
+
+//drag and drop for kenshin image//
+
+function allowDrop(ev) {
+    ev.preventDefault();
+}
+
+function drag(ev) {
+    ev.dataTransfer.setData("text", ev.target.id);
+}
+
+function drop(ev) {
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData("text");
+    ev.target.appendChild(document.getElementById(data));
+}
