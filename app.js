c7fbdef1ac34d70c70c95678c0296c1f2cf911a7
benzed3 <benzed3@gmail.com>
2021-12-06 16:00:32 -0500

    deleted code

--- app.js
+++ app.js
@@ -40,16 +40,3 @@ blackrook.addEventListener("touchend", function (ev) {
 
 //drag and drop for kenshin image//
 
-function allowDrop(ev) {
-    ev.preventDefault();
-}
-
-function drag(ev) {
-    ev.dataTransfer.setData("text", ev.target.id);
-}
-
-function drop(ev) {
-    ev.preventDefault();
-    var data = ev.dataTransfer.getData("text");
-    ev.target.appendChild(document.getElementById(data));
-}
